f971221fd53a842347a7c46e879765ffc14c57ab
Jan Mudry <jairek@o2.pl>
2011-11-03 19:47:27 +0100

    css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,37 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Fortune.delete_all
-Fortune.delete_all
 Rate.delete_all
+User.delete_all
+
 require Rails.root.join('db', 'faker_pl')
 
 Fortune.populate(100..200 ) do |fortune |
   fortune.body = Populator.sentences(1)
   fortune.author =  Faker::Name.first_name + " " + Faker::Name.last_name
 end
+
+10.times{
+
+  u=User.new
+  u.name = Faker::Name.first_name
+
+  u.save
+}
+
+2000.times{
+  r=Rate.new
+  r.rater_id=rand(10)+1;
+  r.rateable_id=rand(190)+1
+  r.rateable_type = "Fortune"
+  r.stars=rand(5)+1
+  r.save
+}
+
+500.times{
+   c=Comment.new
+   c.fortune_id=rand(190)+1
+   c.author= Faker::Name.first_name
+   c.body = Populator.sentences(1)
+  c.save
+}
